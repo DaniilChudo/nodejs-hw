681939a9e8d89dac74b4590616ac390879ee56f3
DaniilChudo <daniilpro1994@gmail.com>
2026-05-09 20:17:53 +0200

    fix2

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,10 @@
 import { HttpError } from 'http-errors';
 
-export const errorHandler = (err, req, res) => {
+export const errorHandler = (err, req, res, next) => {
+  if (next) {
+    // Це просто щоб заспокоїти ESLint, ми не викликаємо next() насправді
+  }
+
   if (err instanceof HttpError) {
     return res.status(err.status).json({
       message: err.message,
