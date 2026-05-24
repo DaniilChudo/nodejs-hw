2ff80ccbed4f2c29fa5eb8f91727e6ab5c9c2af2
DaniilChudo <daniilpro1994@gmail.com>
2026-05-24 14:02:23 +0200

    7

---
+++ src/middleware/authenticate.js
@@ -0,0 +1,33 @@
+import createError from 'http-errors';
+import { Session } from '../models/session.js';
+import { User } from '../models/user.js';
+
+export const authenticate = async (req, res, next) => {
+  try {
+    const { accessToken } = req.cookies;
+
+    if (!accessToken) {
+      return next(createError(401, 'Missing access token'));
+    }
+
+    const session = await Session.findOne({ accessToken });
+    if (!session) {
+      return next(createError(401, 'Session not found'));
+    }
+
+    const isTokenExpired = new Date() > new Date(session.accessTokenValidUntil);
+    if (isTokenExpired) {
+      return next(createError(401, 'Access token expired'));
+    }
+
+    const user = await User.findById(session.userId);
+    if (!user) {
+      return next(createError(401));
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
