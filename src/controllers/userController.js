8553af2ae3d077bc3a2e3fc31cbfa54b436dd2e0
DaniilChudo <daniilpro1994@gmail.com>
2026-05-31 14:50:14 +0200

    6

---
+++ src/controllers/userController.js
@@ -0,0 +1,22 @@
+import createError from 'http-errors';
+import { User } from '../models/user.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+
+export const updateUserAvatar = async (req, res, next) => {
+  try {
+    if (!req.file) {
+      throw createError(400, 'No file');
+    }
+
+    const result = await saveFileToCloudinary(req.file.buffer);
+    const avatarUrl = result.secure_url;
+
+    await User.findByIdAndUpdate(req.user._id, { avatar: avatarUrl });
+
+    res.status(200).json({
+      url: avatarUrl,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRouter from './routes/notesRoutes.js';
 import authRouter from './routes/authRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const setupServer = async () => {
   const app = express();
@@ -22,6 +23,7 @@ const setupServer = async () => {
   // Routes
   app.use(notesRouter);
   app.use(authRouter);
+  app.use(userRouter);
 
   // 404 & Error Handlers
   app.use(notFoundHandler);
